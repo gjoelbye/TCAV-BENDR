014f38b1713f7abe4b00d9b84083f624a9f836fb
AndersGMadsen <andersgj.madsen@gmail.com>
2023-02-27 11:50:21 +0100

    Fix bug

--- notebooks/calculate_activity.py
+++ notebooks/calculate_activity.py
@@ -1,18 +1,21 @@
 import os
 if os.getcwd().split("/")[-1] != 'BENDR-XAI': os.chdir("../")
-from collections import defaultdict
 
 import mne
 import numpy as np
+import matplotlib.pyplot as plt
 from utils import *
 
+from matplotlib import animation
+import matplotlib.cm as cm
+import sys
 from tqdm import tqdm
 from pathlib import Path
 
 subjects_dir, subject, trans, src_path, bem_path = get_fsaverage()
 
 edf_file_path = 'notebooks/S001R03.edf' 
-mmidb_path = Path(r"data/eegmmidb/files")
+mmidb_path = Path(r"/mnt/c/Users/anders/OneDriveDTU/Dokumenter/BENDR-XAI/data/eegmmidb/files")
 parcellation_name = "aparc.a2009s"
 snr = 1.0
 
@@ -39,22 +42,20 @@ pbar = tqdm()
 
 for (dirpath, _, filenames) in os.walk(mmidb_path):
     for filename in filenames:
-        if filename.endswith(".edf"):
-            print(filename)            
-            # raw = get_raw(edf_file_path, filter=True)
-            # annotations = get_annotations(edf_file_path)
-            # annotation_dict = get_window_dict(raw, annotations)
+        if filename.endswith(".edf"):            
+            raw = get_raw(filename, filter=True)
+            annotations = get_annotations(filename)
+            annotation_dict = get_window_dict(raw, annotations)
 
-            # cov = get_cov(raw)
-            # compute_inverse = make_fast_inverse_operator(raw.info, fwd, cov, snr=snr)
+            cov = get_cov(raw)
+            compute_inverse = make_fast_inverse_operator(raw.info, fwd, cov, snr=snr)
 
-            # activity = calculate_activity_per_label(annotation_dict, labels, compute_inverse)
-            # dataset_activity[dirpath.split('/')[-1]][filename[:-4]] = activity
+            activity = calculate_activity_per_label(annotation_dict, labels, compute_inverse)
+            dataset_activity[dirpath.split('/')[-1]][filename[:-4]] = activity
 
-            # pbar.update(1)
-            # pbar.set_description(filename[:-4])
+            pbar.update(1)
+            pbar.set_description(filename[:-4])
 
 pbar.close()
-dataset_activity = dict(dataset_activity)
 
 np.save("mmidb_{}_{}".format(parcellation_name, str(round(snr, 1))), dataset_activity)
